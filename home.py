58c77c7bac6e669b3697f0c500b9370a9cbcf26d
Real-duck <89462645+Real-duck@users.noreply.github.com>
2022-10-12 11:53:56 +0200

    Update home.py

--- home.py
+++ home.py
@@ -30,39 +30,34 @@ def home():
 
 
 
-    #st.markdown('## Die Einstellungen ')
-
-    #st.markdown('#### Transformationen')
-    #st.markdown(
-    #    '*Mit diesen Methoden kann das Ergebnis leicht überprüft werden. Der Datensatz kann mit den Ergebnissen auch erweitert werden.*')
-
-    #st.markdown('___')
-    #st.markdown('___Threshold___:')
-    #st.markdown('Beim Thresholding werden Vordergrundpixel vom Hintergrund seperiert. '
-    #            ' Die Pixel, die zum Vordergrund gehören sind Weiß, die, die zum Hintergrund gehören sind Schwarz. [Mehr zum benutzten Verfahren](https://www.wikiwand.com/en/Otsu%27s_method)')
-    # st.image(Image.open('images/threshold_edge_mask.png'), caption='Thresholding für rote Blutkörperchen',
-    #             width=400)
-    # st.markdown('___')# 
-
-    # st.markdown('___Hough Transform___:')
-    # st.markdown(
-    #     'Diese Methode kann spezifische Formen erkennen. Da Blutkörperchen oft kreisförmig sind, wird hier Circle Hough Transform verwendet. '
-    #     'Das Ergebnis kann für das Zählen einer Blutkörperchen Art benutzt werden. [Mehr zum benutzten Verfahren](https://sbme-tutorials.github.io/2021/cv/notes/4_week4.html)')
-    # st.image(Image.open('images/hough_transform.png'), caption='Hough Transform für rote Blutkörperchen',
-    #             width=400)
-    # st.markdown('___')
-
-    # st.markdown('___Component labeling___:')
-    # st.markdown(
-    #     'Dieser Algorithmus erkennt verbundene Objekte in einem binären Bild, also das Ergebnis der Segmentation.'
-    #     'Component labeling kann auch für das Zählen benutzt werden. [Mehr zum benutzten Verfahren] (https://pyimagesearch.com/2021/02/22/opencv-connected-component-labeling-and-analysis/)')
-    # st.image(Image.open('images/component_labeling.png'), caption='Component labeling für rote Blutkörperchen',
-    #             width=400)
-    # st.markdown('___')
-
-    # st.markdown('___Distance transform___:')
-    # st.markdown(
-    #     'Diese Methode nimmt ein binäres Bild und gibt ein grayscale Bild zurück, das die Distanz zwischen den Pixeln repräsentiert. '
-    #     'Je weiter ein Pixel vom Rand entfernt ist, desto heller wird er sein. [Mehr zum benutzten Verfahren](https://homepages.inf.ed.ac.uk/rbf/HIPR2/distance.htm)')
-    # st.image(Image.open('images/distance_transform.png'), caption='Distance transform für rote Blutkörperchen',
-    #             width=400)
+    st.markdown('## Die Einstellungen ')
+    st.markdown('#### Transformationen')
+    st.markdown(
+        '*Mit diesen Methoden kann das Ergebnis leicht überprüft werden. Der Datensatz kann mit den Ergebnissen auch erweitert werden.*')
+    st.markdown('___')
+    st.markdown('___Threshold___:')
+    st.markdown('Beim Thresholding werden Vordergrundpixel vom Hintergrund seperiert. '
+                ' Die Pixel, die zum Vordergrund gehören sind Weiß, die, die zum Hintergrund gehören sind Schwarz. [Mehr zum benutzten Verfahren](https://www.wikiwand.com/en/Otsu%27s_method)')
+    st.image(Image.open('images/threshold_edge_mask.png'), caption='Thresholding für rote Blutkörperchen',
+                width=400)
+    st.markdown('___')# 
+    st.markdown('___Hough Transform___:')
+    st.markdown(
+        'Diese Methode kann spezifische Formen erkennen. Da Blutkörperchen oft kreisförmig sind, wird hier Circle Hough Transform verwendet. '
+        'Das Ergebnis kann für das Zählen einer Blutkörperchen Art benutzt werden. [Mehr zum benutzten Verfahren](https://sbme-tutorials.github.io/2021/cv/notes/4_week4.html)')
+    st.image(Image.open('images/hough_transform.png'), caption='Hough Transform für rote Blutkörperchen',
+                width=400)
+    st.markdown('___')
+    st.markdown('___Component labeling___:')
+    st.markdown(
+        'Dieser Algorithmus erkennt verbundene Objekte in einem binären Bild, also das Ergebnis der Segmentation.'
+        'Component labeling kann auch für das Zählen benutzt werden. [Mehr zum benutzten Verfahren] (https://pyimagesearch.com/2021/02/22/opencv-connected-component-labeling-and-analysis/)')
+    st.image(Image.open('images/component_labeling.png'), caption='Component labeling für rote Blutkörperchen',
+                width=400)
+    st.markdown('___')
+    st.markdown('___Distance transform___:')
+    st.markdown(
+        'Diese Methode nimmt ein binäres Bild und gibt ein grayscale Bild zurück, das die Distanz zwischen den Pixeln repräsentiert. '
+        'Je weiter ein Pixel vom Rand entfernt ist, desto heller wird er sein. [Mehr zum benutzten Verfahren](https://homepages.inf.ed.ac.uk/rbf/HIPR2/distance.htm)')
+    st.image(Image.open('images/distance_transform.png'), caption='Distance transform für rote Blutkörperchen',
+                 width=400)
